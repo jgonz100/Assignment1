f315523aa8610ad5d050633f3867d3d9d32ca090
jgonz100 <52713480+jgonz100@users.noreply.github.com>
2019-09-26 12:55:39 -0700

    Added ECons to Interpreter.hs

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -37,6 +37,7 @@ evalCBN (EHd e) = case (evalCBN e) of
 evalCBN (ETl e) = case (evalCBN e) of
         (ECons e1 e2) -> evalCBN (e2)
         (ENil) -> ENil
+evalCBN (ECons e1 e2) = ECons (evalCBN e1) (evalCBN e2)
 evalCBN (EFix e) = evalCBN (EApp e (EFix e))
 evalCBN (EMinusOne e) = case (evalCBN e) of
     ENat0 -> ENat0
